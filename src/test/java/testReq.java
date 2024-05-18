03bef8f0e0ad77feea861dcddd2fa7c59862e2e1
MoiseiMarianaQA <moisei.maryana@gmail.com>
2024-05-18 14:52:41 +0300

    post commit

--- src/test/java/testReq.java
+++ src/test/java/testReq.java
@@ -1,17 +1,46 @@
+import io.qameta.allure.Allure;
+import io.restassured.response.Response;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
+import static io.restassured.RestAssured.*;
+import static io.restassured.RestAssured.get;
 
 import static io.restassured.RestAssured.given;
+import static java.lang.reflect.Array.get;
+import static utils.Steps.GET;
+import static utils.Steps.POST;
+import static utils.Steps.isstatuscodevalid;
 
+import utils.Steps.*;
 public class testReq {
+    @BeforeSuite
+    public void setUP(){
+        baseURI="https://reqres.in/api";
+    }
     @Test
 
     public void testListUsers() {
-        given()
-                .when()
-                .get("https://reqres.in/api/users?page=2")
-                .then()
-                .assertThat()
-                .statusCode(200);
-
+        String url = "/users?page=2";
+        Allure.addAttachment("URL", url);
+        Response response = GET(url);
+        isstatuscodevalid(response,200);
     }
+    @Test
+    public void getSingleUser(){
+        String url = "/users/2";
+        Allure.addAttachment("URL", url);
+        Response response = GET(url);
+        isstatuscodevalid(response,200);
+    }
+    @Test
+    public void testCreate(){
+        String url ="/users";
+        String body = "{\n" +
+                "    \"name\": \"morpheus\",\n" +
+                "    \"job\": \"leader\"\n" +
+                "}";
+        Response response = POST(body, url);
+       isstatuscodevalid(response, 201);
+    }
+
 }
---
+++ src/test/java/utils/Steps.java
@@ -0,0 +1,31 @@
+package utils;
+
+import io.qameta.allure.Allure;
+import io.qameta.allure.Step;
+import io.restassured.response.Response;
+import static io.restassured.RestAssured.*;
+import static io.restassured.RestAssured.get;
+public class Steps {
+    @Step
+
+    public static Response GET(String endpoint) {
+        Allure.addAttachment("URL", baseURI + endpoint);
+        Response response = get(endpoint);
+        Allure.addAttachment("Response body", response.body().prettyPrint());
+        Allure.addAttachment("Status code", String.valueOf(response.statusCode()));
+        return response;
+    }
+    @Step
+    public static void isstatuscodevalid(Response response, int expectedStatuscode){
+        response.then().assertThat().statusCode(expectedStatuscode);
+    }
+    @Step
+    public static Response POST(String body, String endpoint ){
+        Allure.addAttachment("URL", baseURI+endpoint);
+        Allure.addAttachment("Request body", body);
+       Response response=  given().body(body).post(endpoint);
+        Allure.addAttachment("Status code", String.valueOf(response.statusCode()));
+        Allure.addAttachment("Response body", response.body().prettyPrint());
+       return response;
+    }
+}
